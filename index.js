e2cb6cbae82b1c6779083ac6791977ef3ba68ac2
Supermyskin <supermyskin@abv.bg>
2026-01-21 12:24:52 +0200

    Started working on regroles command and getting userID from the message

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { Client, IntentsBitField, REST, Routes, Events, GatewayIntentBits } = require('discord.js');
+const { Client, IntentsBitField, REST, Routes, Events, GatewayIntentBits, PermissionsBitField } = require('discord.js');
 require('dotenv').config();
+const fs = require("fs");
 
 const client = new Client({
     intents: [
@@ -13,13 +14,23 @@ const client = new Client({
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 client.on('interactionCreate', (interaction) => {
+    const userId = interaction.user.id;
+
     if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === 'ping') {
-        interaction.reply('Pong!');
+        interaction.reply("Pong!")
+    }
+    else if (interaction.commandName === 'regroles') {
+        if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator) == true) {
+            interaction.reply("✅ The roles should be registered, but I have yet to add it as a feture :(");
+        }
+        else {
+            interaction.reply("❌ You must have Administrator privilages to use this command.");
+        }
     }
 });
 
 client.once("clientReady", () => {
-  console.log("SideQuestord is online!");
+    console.log("SideQuestord is online!");
 });
